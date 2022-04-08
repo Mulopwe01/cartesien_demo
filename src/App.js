8f5f222844b837b51f219852ee6bcab55e5c89ac
Mulopwe01 <songaelie@gmail.com>
2022-04-08 11:52:12 +0300

    changes

--- src/App.js
+++ src/App.js
@@ -6,9 +6,8 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-       <h2>MAINTENANCE</h2>
         <p>
-        le deploiement, c'est bien passe. nous avancons et verrons ce qui nous attend
+          SOBAMA
         </p>
       </header>
     </div>
